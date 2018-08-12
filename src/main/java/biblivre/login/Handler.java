75a564c4d7662f244a6407b6a3b14872ec73f4e9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-08-12 18:01:15 -0300

    Evite bloquear a aplicação procurando por atualizações

--- src/main/java/biblivre/login/Handler.java
+++ src/main/java/biblivre/login/Handler.java
@@ -73,13 +73,6 @@ public class Handler extends AbstractHandler {
 			if (atps.isAdmin()) {
 				boolean warningPassword = password.toLowerCase().equals("abracadabra");
 				request.setSessionAttribute(schema, "system_warning_password", warningPassword);
-
-				String latestVersion = Updates.checkUpdates();
-				latestVersion = StringUtils.chomp(latestVersion);
-
-				if (StringUtils.isNotBlank(latestVersion) && !latestVersion.equals(Constants.BIBLIVRE_VERSION) && NaturalOrderComparator.NUMERICAL_ORDER.compare(latestVersion, Constants.BIBLIVRE_VERSION) > 0) {
-					request.setSessionAttribute(schema, "system_warning_new_version", latestVersion);
-				}
 			}
 
 			if (!schema.equals(Constants.GLOBAL_SCHEMA)) {
